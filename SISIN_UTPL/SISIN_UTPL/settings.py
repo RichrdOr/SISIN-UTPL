dd037efe6ea6305e98987ff02f3cf251a4338f22
RichrdOr <raortiz20@utpl.edu.ec>
2025-12-22 16:26:49 -0500

    nuevas apps

--- SISIN_UTPL/SISIN_UTPL/settings.py
+++ SISIN_UTPL/SISIN_UTPL/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    
+    'polizas',
+    'siniestros',
+    'usuarios',
+    'reportes',
 ]
 
 MIDDLEWARE = [
