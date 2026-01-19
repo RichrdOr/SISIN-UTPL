c469801def66b64be53db90259ed08886efd9a35
ANITA-16 <anacristinaguamangonzalez@gmail.com>
2026-01-19 01:47:25 -0500

    dashboard bonito

--- SISIN_UTPL/SISIN_UTPL/settings.py
+++ SISIN_UTPL/SISIN_UTPL/settings.py
@@ -39,9 +39,11 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'django_fsm',
+    # Apps del proyecto
+    'usuarios',
     'polizas',
     'siniestros',
-    'usuarios',
+    'notificaciones',  # ← AGREGAR ESTA LÍNEA
     'reportes',
 ]
 
@@ -83,7 +85,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'sisis_utpl',
         'USER': 'postgres',
-        'PASSWORD': 'password',
+        'PASSWORD': 'PIZZA3',
         'HOST': 'localhost',
         'PORT': '5432',
     }
